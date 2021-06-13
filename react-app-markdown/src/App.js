2c49f17c832c05024b3ff96988ed538f58230bf6
USER <USER@DESKTOP-FNDODER>
2021-06-13 07:31:11 +0300

    Add project files.

---
+++ react-app-markdown/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Badge from "react-bootstrap/Badge";
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+			<div className="container">
+			<header>
+			<h1 style={{ color: 'red' }}>Hello World!</h1>
+			</header>
+			
+			</div>
+
+			</div>
+		);}
+}
